a09e24f982032dc9b3a4d75051fff5ebb1aa8387
Denis O'Connor <denis.oconnor@eprize.com>
2014-08-13 08:18:26 -0500

    Correct comments relating to config values.

--- config/example.config.db.php
+++ config/example.config.db.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-    'db_username'=>'',     // Host name         TODO - need value
-    'db_password'=>'',     // Mysql username    TODO - need value
-    'db_host'=>''          // Mysql password    TODO - need value
+    'db_username'=>'',     // Mysql username    TODO - need value
+    'db_password'=>'',     // Mysql password    TODO - need value
+    'db_host'=>''          // Host name         TODO - need value
 );
