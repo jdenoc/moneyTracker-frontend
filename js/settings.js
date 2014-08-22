7147c810296c4e66430ab83297bb5a2436c24064
jdenoc <jdenoc@gmail.com>
2014-08-21 19:21:23 -0500

    Finalised some JavaScript functions.

--- js/settings.js
+++ js/settings.js
@@ -13,7 +13,16 @@ var accounts = {
             data: { type: 'get_account_data' },
             beforeSend:function(){},
             success:function(accountData){
-                $('#accounts').append(accountData);
+                $('#account_settings').append(accountData);
+                $('.account_setting h3').bind('click', accounts.types.display);
+                $('.add_type').bind('click', accounts.types.save);
+                $('.save_type').bind('click', accounts.types.save);
+                $('.edit_type').bind('click', accounts.types.edit);
+                $('.disable_type').bind('click', accounts.types.disable);
+                $('.account_type').each(function(idx, obj){
+                    var typeVal = $(obj).children('input[type="hidden"]').remove();
+                    $(obj).children('label').children('select').val(typeVal.val());
+                });
                 loading.end();
             },
             error:function(){
@@ -23,19 +32,55 @@ var accounts = {
     },
     add: function(){
         // TODO - create a new account
+        alert('this doesn\'t work yet.');
     },
     disable: function(){
         // TODO - disable account
+        alert('this doesn\'t work yet.');
     },
     types: {
         display: function(){
-            // TODO - this will just be a show/hide toggle
+            var typeData = accounts.types.initHandler($(this)[0]);
+            var element = "#"+typeData.accountID+" ul .account_type";
+            if($(element).is(':visible')){
+                $(element).slideUp();
+            } else {
+                $(element).slideDown();
+            }
+        },
+        add: function(){
+            // TODO - this will open a model
+        },
+        edit: function(){
+            var typeData = accounts.types.initHandler($(this)[0]);
+            var element = "#"+typeData.typeID;
+            $(element+' label input').prop('readonly', false);
+            $(element+' label select').prop('disabled', false);
+            $(element+' .edit_type').hide();
+            $(element+' .save_type').show();
         },
         save: function(){
             // TODO - add/update account type
+            alert('this doesn\'t work yet.');
+            var typeData = accounts.types.initHandler($(this)[0]);
+            if(typeof typeData.typeID == 'undefined'){
+                // TODO - we're adding a new account type here
+                // TODO - cause modal to appear
+            } else {
+                // TODO - we're going to update an existing account type
+                // TODO - cause modal to appear
+            }
         },
         disable: function(){
             // TODO - disable account type
+            var typeData = accounts.types.initHandler($(this)[0]);
+            alert('this doesn\'t work yet.');
+        },
+        initHandler: function(element){
+            var accountTypeData = {};
+            accountTypeData.accountID = $(element).parents('tr').prop('id');
+            accountTypeData.typeID = $(element).parents('li').prop('id');
+            return accountTypeData;
         }
     }
 };
