12818ac69cde520de5e1ee34ad5013a84648eadc
jdenoc <jdenoc@gmail.com>
2014-09-07 00:43:49 -0500

    * Added a check to accounts.display() to make sure we don't throw an "undefined" error. * Replaced all binds with delegates that are called in the $(function()). * Updated logic to handle creating dialogues for adding new account types. * Updated some logic with saving account types.

--- js/settings.js
+++ js/settings.js
@@ -13,12 +13,6 @@ var accounts = {
             beforeSend:function(){},
             success:function(accountData){
                 $('#account_settings').append(accountData);
-                $('.account_setting h3').bind('click', accounts.types.display);
-                $('.add_type').bind('click', accounts.types.save);
-                $('.save_type').bind('click', accounts.types.save);
-                $('.cancel_type').bind('click', accounts.types.cancel);
-                $('.edit_type').bind('click', accounts.types.edit);
-                $('.disable_type').bind('click', accounts.types.disable);
                 $('.account_type').each(function(idx, obj){
                     var typeData = accounts.types.initHandler(obj);
                     typeData.accountID = typeData.accountID.replace('account_setting_', '');
@@ -27,7 +21,9 @@ var accounts = {
                     $(typeOptions).each(function(typeIdx, typeObj){
                         selectOptions += '<option value="'+typeObj+'">'+typeObj.substring(0,1).toUpperCase()+typeObj.substring(1)+'</option>'+"\n";
                     });
-                    $(obj).children('label').children('select').append(selectOptions).val( types[typeData.accountID][typeData.typeID] );
+                    if(typeof types[typeData.accountID] != 'undefined'){
+                        $(obj).children('label').children('select').append(selectOptions).val( types[typeData.accountID][typeData.typeID] );
+                    }
                 });
                 loading.end();
             },
@@ -47,6 +43,7 @@ var accounts = {
     },
     types: {
         tempData:{},
+        newButton: "<li class='account_type add_type btn'>Add Account Type</li>\n",
         display: function(){
             var typeData = accounts.types.initHandler($(this)[0]);
             var element = "#"+typeData.accountID+" ul .account_type";
@@ -57,22 +54,35 @@ var accounts = {
                 $(element+' .save_type').hide();
                 $(element+' .cancel_type').hide();
                 $(element+' .edit_type').show();
+                if(!$(element+':last-child').hasClass('add_type')){
+                    $(element+':last-child').remove();
+                    $("#"+typeData.accountID+" ul").append(accounts.types.newButton);
+                }
             } else {
                 $(element).slideDown();
             }
         },
         add: function(){
             var typeData = accounts.types.initHandler($(this)[0]);
-            $(this).remove();       // TODO - test to see if this works
+            $(this).remove();
+            var selectOptions = '';
+            $(typeOptions).each(function(typeIdx, typeObj){
+                selectOptions += '<option value="'+typeObj+'">'+typeObj.substring(0,1).toUpperCase()+typeObj.substring(1)+'</option>'+"\n";
+            });
             var display = '<li class="account_type">';
             display += '    <label>Name:<input type="text" name="type_name" class="form-control" /></label>';
             display += '    <label>Last Digits:<input type="text" name="last_digits" class="form-control" /></label>';
-            display += '    <label>Type: <select name="type" class="form-control" >'+$type_options+'</select></label>';  // TODO - figure out how to display select options
+            display += '    <label>Type: <select name="type" class="form-control" >'+selectOptions+'</select></label>';
             display += '    <button type="button" class="btn btn-default type_button save_type">Save</button>';
             display += '    <button type="button" class="btn btn-default type_button cancel_type">Cancel</button>';
             display += "</div></li>"+"\n";
-            display += "<li class='account_type add_type btn'>Add Account Type</li>"+"\n";
             $('#'+typeData.accountID+' ul').append(display);
+            var element = "#"+typeData.accountID+" ul li:last-child";
+            $(element).show();
+            $(element+' label input').prop('readonly', false);
+            $(element+' label select').prop('disabled', false);
+            $(element+' .save_type').show();
+            $(element+' .cancel_type').show();
 
         },
         edit: function(){
@@ -86,9 +96,11 @@ var accounts = {
         },
         cancel: function(){
             var typeData = accounts.types.initHandler($(this)[0]);
-            if(typeof typeData.typeID == 'undefined'){
-                // TODO - remove section. This is a "potential" added type.
-                $(this).remove();   // TODO - Let's see if this actually works.
+            if(typeData.typeID == ''){
+                $(this).parent().remove();
+                $('#'+typeData.accountID+' ul').append(accounts.types.newButton);
+                $('#'+typeData.accountID+' ul li:last-child()').show();
+
             } else {
                 var element = "#"+typeData.typeID;
                 $(element+' label input').prop('readonly', true);
@@ -102,13 +114,20 @@ var accounts = {
             // TODO - add/update account type
             alert('this doesn\'t work yet.');
             var typeData = accounts.types.initHandler($(this)[0]);
-            if(typeof typeData.typeID == 'undefined'){
+            if(typeData.typeID == ''){
                 // TODO - we're adding a new account type here
-                // TODO - cause modal to appear
+                accounts.types.tempData.type_name = $('#'+typeData.accountID+' ul li:last-child() input[name="type_name"]').val();
+                accounts.types.tempData.last_digits = $('#'+typeData.accountID+' ul li:last-child() input[name="last_digits"]').val();
+                accounts.types.tempData.type = $('#'+typeData.accountID+' ul li:last-child() select').val();
             } else {
                 // TODO - we're going to update an existing account type
-                // TODO - cause modal to appear
+                accounts.types.tempData.type_name = $('#'+typeData.typeID+' input[name="type_name"]').val();
+                accounts.types.tempData.last_digits = $('#'+typeData.typeID+' input[name="last_digits"]').val();
+                accounts.types.tempData.type = $('#'+typeData.typeID+' select').val();
             }
+            // TODO - check if there are any empty values. If so, then don't actually save anything.
+            // TODO - Write ajax to save account type
+            // TODO - refresh page
         },
         disable: function(){
             // TODO - disable account type
@@ -129,4 +148,11 @@ $(function(){
     loading.img = 'imgs/loader.gif';
     loading.start();
     accounts.display();
+    var accountSettings = $('#account_settings');
+    accountSettings.delegate('.add_type', 'click', accounts.types.add);
+    accountSettings.delegate('.save_type', 'click', accounts.types.save);
+    accountSettings.delegate('.cancel_type', 'click', accounts.types.cancel);
+    accountSettings.delegate('.edit_type', 'click', accounts.types.edit);
+    accountSettings.delegate('.disable_type', 'click', accounts.types.disable);
+    accountSettings.delegate('h3', 'click', accounts.types.display);
 });
