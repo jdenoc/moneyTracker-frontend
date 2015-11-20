8f3a5175ff02de8544efa04b0dd97409335b391d
jdenoc <jdenoc@gmail.com>
2015-11-19 21:40:53 -0600

    Updated css/attachment.php to take into account changes to config/example.config.db.php

--- css/attachment.php
+++ css/attachment.php
@@ -12,7 +12,8 @@ if(empty($_SESSION['email'])){
 require_once(__DIR__.'/../Lib/php/PDO_Connection.php');
 
 $id = intval($_REQUEST['id']);
-$db = new PDO_Connection('jdenoc_money_tracker', __DIR__.'/../config/config.db.php');
+$db_config = require(__DIR__.'/../config/config.db.php');
+$db = new PDO_Connection($db_config['database'], $db_config['username'], $db_config['password'], $db_config['hostname']);
 
 $attachment = $db->getRow("SELECT * FROM attachments WHERE id=:attachment_id;", array('attachment_id'=>$id));
 $md5 = include_once(__DIR__ . '/../config/config.md5.php');
