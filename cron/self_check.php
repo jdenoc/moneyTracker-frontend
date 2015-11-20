3e60ef94aac1d485a58c36e4fee0408845ef666d
jdenoc <jdenoc@gmail.com>
2015-11-20 08:16:08 -0600

    Updated cron/self_check.php to take into account changes to config/example.config.db.php

--- cron/self_check.php
+++ cron/self_check.php
@@ -10,7 +10,8 @@
  */
 
 require_once(__DIR__.'/../Lib/php/PDO_Connection.php');
-$db = new PDO_Connection('jdenoc_money_tracker', __DIR__.'/../config/config.db.php');
+$db_config = require(__DIR__.'/../config/config.db.php');
+$db = new PDO_Connection($db_config['database'], $db_config['username'], $db_config['password'], $db_config['hostname']);
 
 $accounts = $db->getAllRows("SELECT * FROM accounts");
 $msg = "";
