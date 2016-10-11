cade061030e543d95b39e42167569e9c99a2e1b6
jdenoc <jdenoc@gmail.com>
2016-10-10 23:34:11 -0500

    Uploaded files are now uploaded to the system designated "tmp" folder, and not one within the project.

--- includes/file_upload.php
+++ includes/file_upload.php
@@ -3,14 +3,12 @@
  * Used to handle initial file uploads, so we don't loose the file after the upload session is complete.
  */
 
-$tmp_dir = __DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."tmp".DIRECTORY_SEPARATOR;
 if(isset($_FILES["file"])){
 //Filter the file types , if you want.
     if ($_FILES["file"]["error"] > 0){
         echo "Error: " . $_FILES["file"]["error"];
     } else {
-        move_uploaded_file($_FILES["file"]["tmp_name"], $tmp_dir.str_replace(' ', '_', $_FILES["file"]["name"]));
+        move_uploaded_file($_FILES["file"]["tmp_name"], sys_get_temp_dir().DIRECTORY_SEPARATOR.str_replace(' ', '_', $_FILES["file"]["name"]));
         echo json_encode(str_replace(' ', '_', $_FILES["file"]["name"]));
     }
-
 }
