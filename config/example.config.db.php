6446272229a6da939e13685e1e1210ec2b756185
jdenoc <jdenoc@gmail.com>
2016-12-02 16:44:27 -0600

    * Added a new composer package "vlucas/phpdotenv" to replace the usage of config files. * Created a new file, includes/initialise_env.php, to load the new "environment" values at the start.

--- config/example.config.db.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-    'hostname'=>'',     // Database Hostname    TODO - need value
-    'database'=>'',     // Database Name        TODO - need value
-    'username'=>'',     // Database username    TODO - need value
-    'password'=>''      // Database password    TODO - need value
-);
--- config/example.config.session.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * User: denis
- * Date: 3/30/14
- * Time: 1:30 PM
- */
-
-return "";      // TODO - need value
