5e6dc2b13e87fec38a38bf7c6b5a28edf3507440
jdenoc <jdenoc@gmail.com>
2016-12-06 01:19:49 -0600

    Created functions to handle exceptions and in turn log and respond that a fatal error has occurred.

--- includes/request_data.php
+++ includes/request_data.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once(__DIR__ . '/ProcessData.php');
+set_exception_handler('exception_handler');
 
 $post = false;
 $post_data = array();
@@ -94,9 +95,7 @@ switch($_REQUEST['type']){
 $json_response = ProcessData::make_call(ProcessData::get_base_rest_url().$uri, $post, $post_data);
 
 if(!$response_array = json_decode($json_response, true)){
-    error_log(ProcessData::ERROR_LABEL."failed JSON response\n".$json_response);
-    header("HTTP/1.1 500 Internal Server Error");
-    exit;
+    fatal_error_response("failed JSON response\n".$json_response);
 } else {
     if(empty($response_array['error'])){
         $response = call_user_func(array('ProcessData', $callback), $response_array['result']);
@@ -106,4 +105,22 @@ if(!$response_array = json_decode($json_response, true)){
         header("HTTP/1.1 400 Bad Request");
         exit;
     }
+}
+
+/**
+ * causes the script to die, with a 500 error and then log an error.
+ * @param string $log_msg
+ */
+function fatal_error_response($log_msg){
+    error_log(ProcessData::ERROR_LABEL.$log_msg);
+    header("HTTP/1.1 500 Internal Server Error");
+    exit;
+}
+
+/**
+ * Handles any exceptions that may occur
+ * @param Exception $e
+ */
+function exception_handler(Exception $e){
+    fatal_error_response($e);
 }
